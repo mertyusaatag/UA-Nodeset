a8bd88b4d010c00848e5ca747c3acbe99fe50d0e
Mariusz <mpostol@cas.eu>
2021-05-26 20:45:00 +0200

    Testing using publicly available models #8
    - testing using Address Space Prototyping (asp.exe) Version 6.3.2.23344
    - tested AutoID

---
+++ AutoID/ModelDesign/DoRecoverModelDesign.cmd
@@ -0,0 +1,7 @@
+:: Recovering ModelDesign from UANodeSet OPC UA Model Information
+:: %1 - location of the asp.exe 
+del %1\asp.log  /q
+del %1\asp.warnings.log /q
+%1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.AutoID.NodeSet2.xml" -e "Opc.Ua.AutoID.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/AutoID/"
+xcopy "%1\asp.log" "asp.log" /y
+xcopy "%1\asp.warnings.log" ".\asp.warnings.log" /y /i
