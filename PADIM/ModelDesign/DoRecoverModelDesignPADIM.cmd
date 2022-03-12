4f6673813bc4cc5ab99b59fd3b3ae1b1b41f95d7
mertyusaatag <mertatag0@gmail.com>
2022-03-12 16:59:13 +0300

    Adding new scripts closed#2

--- PADIM/ModelDesign/DoRecoverModelDesignPADIM.cmd
+++ PADIM/ModelDesign/DoRecoverModelDesignPADIM.cmd
@@ -1,9 +1,9 @@
 :: Recovering ModelDesign from UANodeSet OPC UA Model Information
 :: %1 - location of the asp.exe 
-del %1\asp.padim.log  /q
-del %1\asp.padim.warnings.log /q
+del %1\asp.log  /q
+del %1\asp.warnings.log /q
 %1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.IRDI.NodeSet2.xml" "..\Opc.Ua.PADIM.NodeSet2.xml" -e "Opc.Ua.PADIM.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/PADIM/"
-xcopy %1\asp.padim.log asp.log /y
-xcopy %1\asp.padim.warnings.log .\asp.warnings.log /y /i
+xcopy %1\asp.log asp.padim.log /y
+xcopy %1\asp.warnings.log .\asp.padim.warnings.log /y /i
 
 
