10a62a322400f2960674f09e9adeeacb94fe30a0
Mert Yuşa Atağ <69805617+mertyusaatag@users.noreply.github.com>
2022-04-12 23:14:57 +0300

    Model Added For PlasticRubber

---
+++ PlasticsRubber/Extrusion/Calibrator/1.00/ModelDesign/DoRecoverModelDesign.cmd
@@ -0,0 +1,9 @@
+:: Recovering ModelDesign from UANodeSet OPC UA Model Information
+:: %1 - location of the asp.exe 
+del %1\asp.log  /q
+del %1\asp.warnings.log /q
+%1\asp "..\..\..\..\GeneralTypes\1.03\Opc.Ua.PlasticsRubber.GeneralTypes.NodeSet2.xml" "..\..\..\GeneralTypes\1.01\Opc.Ua.PlasticsRubber.Extrusion.GeneralTypes.NodeSet2.xml" "..\Opc.Ua.PlasticsRubber.Extrusion.Calibrator.NodeSet2.xml" -e "Opc.Ua.PlasticsRubber.Extrusion.Calibrator.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/PlasticsRubber/Extrusion/Calibrator/"
+xcopy %1\asp.log asp.log /y
+xcopy %1\asp.warnings.log .\asp.warnings.log /y /i
+
+
