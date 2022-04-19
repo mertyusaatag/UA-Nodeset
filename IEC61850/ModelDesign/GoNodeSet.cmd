e26218bfcb2a2df1f54bbd23f3b167a559158bac
MPostol <mpostol@cas.eu>
2022-04-19 23:18:28 +0200

    Test recovered models against Model Compiler 2.1.1-Bravo+ #97
    - IEC61850 regenerated using  ModelDesign Compiler (mdc.exe) Version: 3.0.0.41853

--- IEC61850/ModelDesign/GoNodeSet.cmd
+++ IEC61850/ModelDesign/GoNodeSet.cmd
@@ -1,20 +1,20 @@
 set COMPILER=mdc
 
-del %1\OOIMC.log  /q
-del %1\OOIMC.warnings.log /q
-"%1\%COMPILER%" -d2 "Opc.Ua.IEC61850-7-3.Model.xml" -cg "Opc.Ua.IEC61850-7-3.csv" -o "IEC61850-7-3"
-xcopy %1\OOIMC.log .\IEC61850-7-3\OOIMC.log /y /i
-xcopy %1\OOIMC.warnings.log .\IEC61850-7-3\OOIMC.warnings.log /y /i
+del %1\mdc.log  /q
+del %1\mdc.warnings.log /q
+"%1\%COMPILER%" compile --d2 "Opc.Ua.IEC61850-7-3.Model.xml" -c "Opc.Ua.IEC61850-7-3.csv" --o2 "IEC61850-7-3"
+xcopy %1\mdc.log .\IEC61850-7-3\mdc.log /y /i
+xcopy %1\mdc.warnings.log .\IEC61850-7-3\mdc.warnings.log /y /i
 
-del %1\OOIMC.log  /q
-del %1\OOIMC.warnings.log /q
-"%1\%COMPILER%" -d2 "Opc.Ua.IEC61850-6.Model.xml" -cg "Opc.Ua.IEC61850-6.csv" -o "IEC61850-6"
-xcopy %1\OOIMC.log .\IEC61850-6\OOIMC.log /y /i
-xcopy %1\OOIMC.warnings.log .\IEC61850-6\OOIMC.warnings.log /y /i
+del %1\mdc.log  /q
+del %1\mdc.warnings.log /q
+"%1\%COMPILER%" compile --d2 "Opc.Ua.IEC61850-6.Model.xml" -c "Opc.Ua.IEC61850-6.csv" --o2 "IEC61850-6"
+xcopy %1\mdc.log .\IEC61850-6\mdc.log /y /i
+xcopy %1\mdc.warnings.log .\IEC61850-6\mdc.warnings.log /y /i
 
-del %1\OOIMC.log  /q
-del %1\OOIMC.warnings.log /q
-"%1\%COMPILER%" -d2 "Opc.Ua.IEC61850-7-4.Model.xml" -cg "Opc.Ua.IEC61850-7-4.csv" -o "IEC61850-7-4"
-xcopy %1\OOIMC.log .\IEC61850-7-4\OOIMC.log /y /i
-xcopy %1\OOIMC.warnings.log .\IEC61850-7-4\OOIMC.warnings.log /y /i
+del %1\mdc.log  /q
+del %1\mdc.warnings.log /q
+"%1\%COMPILER%" compile --d2 "Opc.Ua.IEC61850-7-4.Model.xml" -c "Opc.Ua.IEC61850-7-4.csv" --o2 "IEC61850-7-4"
+xcopy %1\mdc.log .\IEC61850-7-4\mdc.log /y /i
+xcopy %1\mdc.warnings.log .\IEC61850-7-4\mdc.warnings.log /y /i
 
