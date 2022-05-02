68a5196e1579362e033d25d5fa05168d7fb6e611
Mert Yuşa Atağ <69805617+mertyusaatag@users.noreply.github.com>
2022-05-02 23:46:23 +0300

    Bug fix for FDT

--- FDT/ModelDesign/DoRecoverModelDesign.cmd
+++ FDT/ModelDesign/DoRecoverModelDesign.cmd
@@ -2,7 +2,7 @@
 :: %1 - location of the asp.exe 
 del %1\asp.log  /q
 del %1\asp.warnings.log /q
-%1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.FDT.NodeSet2.xml" -e "Opc.Ua.FDT.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/schemas/FDT/1.0/"
+%1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.FDT.NodeSet.xml" -e "Opc.Ua.FDT.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/FDT/"
 xcopy %1\asp.log asp.log /y
 xcopy %1\asp.warnings.log .\asp.warnings.log /y /i
 
