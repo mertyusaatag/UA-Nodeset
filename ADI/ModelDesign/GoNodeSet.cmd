3f3741f5013790819106dcfde2c834f6579805e7
Mert Yuşa Atağ <69805617+mertyusaatag@users.noreply.github.com>
2022-05-09 12:32:32 +0300

    Fix for Adi old model test with ModelCompiler 3.0.0

--- ADI/ModelDesign/GoNodeSet.cmd
+++ ADI/ModelDesign/GoNodeSet.cmd
@@ -2,7 +2,7 @@ set COMPILER=mdc
 
 del %1\mdc.log  /q
 del %1\mdc.warnings.log /q
-"%1\%COMPILER%" compile --d2 "..\OpcUaAdiModel.xml" -c "OpcUaAdiModel.csv" --o2 "ADI-OldModel "
+"%1\%COMPILER%" compile --d2 "..\OpcUaAdiModel.xml" -c "..\OpcUaAdiModel.csv" --o2 "ADI-OldModel "
 xcopy %1\mdc.log .\ADI-OldModel\mdc.log /y /i
 xcopy %1\mdc.warnings.log .\ADI-OldModel\mdc.warnings.log /y /i
 
