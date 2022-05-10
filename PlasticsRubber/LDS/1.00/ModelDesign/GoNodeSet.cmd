9f875af24675db817a015f1934d8400a61135fda
Mert Yuşa Atağ <69805617+mertyusaatag@users.noreply.github.com>
2022-05-10 21:48:19 +0300

    Executed with ModelCompiler-3.0.0  PlasticsRubber/LDS

---
+++ PlasticsRubber/LDS/1.00/ModelDesign/GoNodeSet.cmd
@@ -0,0 +1,7 @@
+set COMPILER=mdc
+
+del %1\mdc.log  /q
+del %1\mdc.warnings.log /q
+"%1\%COMPILER%" compile --d2 "Opc.Ua.PlasticsRubber.LDS.Model.xml" -g "LDS.Model.csv" --o2 "LDS"
+xcopy %1\mdc.log .\LDS\mdc.log /y /i
+xcopy %1\mdc.warnings.log .\LDS\mdc.warnings.log /y /i
